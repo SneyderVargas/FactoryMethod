b694627fd209a3ddb5c96b68245d6498c487e285
Jhonathan Vargas <sneyr845@gmail.com>
2023-10-04 10:49:54 -0500

    Se crea el controlador de pagos

---
+++ FactoryMethod/Controllers/PaymentController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FactoryMethod.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class PaymentController : Controller
+    {
+        [HttpPost]
+        public IActionResult Paiment([FromQuery] string producto)
+        {
+            try
+            {
+                return Ok("Operación exitosa");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
