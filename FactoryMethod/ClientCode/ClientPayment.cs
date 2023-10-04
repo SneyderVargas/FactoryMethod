74557c6aaf2cf627ee51bbd57aaf5aad1fc4cae8
Jhonathan Vargas <sneyr845@gmail.com>
2023-10-04 11:19:34 -0500

    Creamos el cliente de pagos que no permite tener control del proceso

---
+++ FactoryMethod/ClientCode/ClientPayment.cs
@@ -0,0 +1,16 @@
+﻿using FactoryMethod.CreatorFile;
+
+namespace FactoryMethod.ClientCode
+{
+    public class ClientPayment
+    {
+        public void ProcessPayment(CreatorPayment creatorPayment, decimal amount)
+        {
+            creatorPayment.ProcessPayment(amount);
+        }
+        public void RefundPayment(CreatorPayment creatorPayment, decimal amount)
+        {
+            creatorPayment.RefundPayment(amount);
+        }
+    }
+}
