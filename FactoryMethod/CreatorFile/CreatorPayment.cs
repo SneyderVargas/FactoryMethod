4c5000d786752b521f52390c3f182e4dd93e17fe
Jhonathan Vargas <sneyr845@gmail.com>
2023-10-04 11:08:47 -0500

    Se crear el creador de objeto y la logica de negocio que implementa la interfez

---
+++ FactoryMethod/CreatorFile/CreatorPayment.cs
@@ -0,0 +1,21 @@
+﻿using FactoryMethod.Interfeces.Repositories;
+
+namespace FactoryMethod.CreatorFile
+{
+    public abstract class CreatorPayment
+    {
+        public abstract IPaymentFactory PaymentFactory();
+
+        public void ProcessPayment(decimal amount)
+        {
+            var payment = PaymentFactory();
+            payment.ProcessPayment(amount);
+        }
+        public void RefundPayment(decimal amount)
+        {
+            var payment = PaymentFactory();
+            payment.RefundPayment(amount);
+
+        }
+    }
+}
---
+++ FactoryMethod/Interfeces/Repositories/PaymentFactory.cs
@@ -0,0 +1,24 @@
+﻿namespace FactoryMethod.Interfeces.Repositories
+{
+    public class PaymentFactory : IPaymentFactory
+    {
+        private string bankName;
+
+        public PaymentFactory(string bankName)
+        {
+            this.bankName = bankName;
+        }
+
+        public void ProcessPayment(decimal amount)
+        {
+            // Lógica para procesar un pago con el banco especificado
+            Console.WriteLine($"Procesando un pago de {amount:C} con {bankName}");
+        }
+
+        public void RefundPayment(decimal amount)
+        {
+            // Lógica para realizar una devolución con el banco especificado
+            Console.WriteLine($"Realizando una devolución de {amount:C} con {bankName}");
+        }
+    }
+}
